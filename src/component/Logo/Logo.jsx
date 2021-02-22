e9b87d7d305b333a4a482f692190934cf7f1d66e
Timileyin <timileyinoyelekan11@gmail.com>
2021-02-22 05:23:02 +0000

    Folder Structure

---
+++ src/component/Logo/Logo.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import LogoImg from '../../img/logo.svg';
+
+function Logo(){
+    return(
+    
+    <div id='logo'>
+        <LogoImg />
+    </div>
+    
+    )
+}
+
+export default Logo ;
